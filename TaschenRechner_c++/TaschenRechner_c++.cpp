36b7f2ad2ac5e621ad0e42b1a1ef1f309749f19d
A Tunc <106016458+aatunc@users.noreply.github.com>
2023-03-02 14:59:03 +0100

    Projektdateien hinzufügen.

---
+++ TaschenRechner_c++/TaschenRechner_c++.cpp
@@ -0,0 +1,65 @@
+// TaschenRechner_c++.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char op; // Operator
+    double num1, num2; // Operanden
+
+    cout << "Geben Sie den Operator (+, -, *, /) ein: ";
+    cin >> op;
+
+    cout << "Geben Sie die ersten Zahl ein: ";
+    cin >> num1;
+
+    cout << "Geben Sie die zweite Zahl ein: ";
+    cin >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+        case '/':
+            if(num2 == 0)
+            {
+                cout << "Fehler: Division durch Null!";
+            }
+            else
+            {
+                cout << num1 << " / " << num2 << " = " << num1 / num2;
+            }
+            break;
+
+        default:
+            cout << "Ungültiger Operator!";
+            break;
+    }
+
+    return 0;
+}
+
+
+// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
